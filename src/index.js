076b32ed4937c07557a41e0c75620bf38f18a8a7
Atul <dr6217811@gmail.com>
2025-11-29 17:58:38 +0530

    Add morgan package

--- src/index.js
+++ src/index.js
@@ -2,9 +2,12 @@ import express from "express"
 import cors from "cors"
 import { PORT } from "./config/serverConfig.js"
 import apiRouter from "./routes/index.js"
+import morgan from "morgan"
 
 const app = express()
 
+
+app.use(morgan())
 app.use(express.json())
 app.use(express.urlencoded())
 app.use(cors())
