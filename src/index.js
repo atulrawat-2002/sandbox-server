56926e12620bb4332cb49188a243db7f67609f71
Atul <dr6217811@gmail.com>
2025-11-30 09:52:48 +0530

    configured socket.io

--- src/index.js
+++ src/index.js
@@ -3,18 +3,30 @@ import cors from "cors"
 import { PORT } from "./config/serverConfig.js"
 import apiRouter from "./routes/index.js"
 import morgan from "morgan"
+import { Server } from "socket.io"
+import { createServer } from 'node:http'
 
 const app = express()
-
+const server = createServer(app);
+const io = new Server(server, {
+    cors: {
+        origin: '*',
+        methods: ['GET', 'POST'],
+    }
+});
 
 app.use(morgan())
 app.use(express.json())
 app.use(express.urlencoded())
 app.use(cors())
 
+io.on('connection', (socket) => {
+    console.log('a user connected')
+})
+
 app.use("/api", apiRouter)
 
-app.listen(PORT, () => {
+server.listen(PORT, () => {
     console.log(`App is listening on port no. ${PORT}`);
     
 })
